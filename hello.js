0ab5569ca0a5434591ab9a4ce6304167521d58dc
Rob Hess <hessro@oregonstate.edu>
2023-11-09 09:51:01 -0800

    Add code from 11/8 lecture.

---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!")
+console.log("== process.env:", process.env)
+console.log("== process.env.MY_ENV_VARIABLE:", process.env.MY_ENV_VARIABLE)
+
+console.log("== __filename:", __filename)
+console.log("== __dirname:", __dirname)
